e92794ea4b8b83ba64674d2067d9767cd22691fb
Ikey Doherty <ikey@solus-project.com>
2016-02-01 00:24:00 +0000

    Ensure to nuke deltas
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mvem.sh
+++ mvem.sh
@@ -4,4 +4,7 @@ if [[ ! -d "clones" ]]; then
     mkdir "clones"
 fi
 
+find . -name "*.delta.eopkg"|xargs rm -v
+
 find unstable -name "*.eopkg" | grep -v "*.delta" | grep -v "\-dbginfo"|xargs -I{} ln -v {} clones/.
+
