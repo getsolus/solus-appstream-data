1fd78620ce477a0e0f71dbee5a2f3fd84c70e1a8
Joey Riches <josephriches@gmail.com>
2024-01-08 17:18:20 +0000

    Refresh appstream metadata

--- publish.sh
+++ publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [[ ! -d "work/output" ]]; then
+    echo "No output directory found, have you ran appstream-builder?"
+fi
+
 pushd work/output
 
-mkdir mirror
-mkdir mirror/source
+if [[ ! -d "mirror/source" ]]; then
+    mkdir -p mirror/source
+fi
 
 for i in 112x63  224x126 1248x702  624x351 1504x846 752x423; do
     cp -R $i mirror/.
